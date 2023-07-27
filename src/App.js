3e51dd3e8287dff54336c9d7a6819bd9b4534b35
manish7017 <manish84398@gmail.com>
2023-07-27 14:03:56 +0530

    update

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
-  const
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares) {
