25dbda12c6981936566829db85891e30231b3afb
manish7017 <manish84398@gmail.com>
2023-07-27 10:14:43 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
 export default function Square() {
-  return <button className="square">X</button>;
+  return (
+    <div>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </div>
+  );
 }
